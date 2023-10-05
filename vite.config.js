9a7f7c96666fd0ecce5f239de616d5bad421ecec
unknown <sami.huttu@yahoo.com>
2023-10-05 18:00:23 +0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/bookstore/', // add this property and the value is the name of your repository
+  plugins: [react()],
+})
